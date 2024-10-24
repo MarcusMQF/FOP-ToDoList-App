274b2b22dd935aad3ced814fef17dc06fd898cab
Marcus <marcusmah6969@gmail.com>
2024-10-24 15:35:10 +0800

    first commit

---
+++ TodoList.java
@@ -0,0 +1,19 @@
+import java.awt.event.MouseAdapter;
+import javax.swing.JPanel;
+import javax.swing.JButton;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
+
+public class TodoList{
+    public static void main(String[] args){
+        
+    }
+}
+
+class Task{
+
+}
+
+class Interface{
+
+}
